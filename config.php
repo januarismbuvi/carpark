cfd2beeacc14ee6445083259d5841e54c64c9c48
januarismbuvi <50013301+januarismbuvi@users.noreply.github.com>
2020-11-16 09:13:59 +0000

    Add files via upload

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ define('DB_NAME','carrental');
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+//$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ATTR_EXCEPTION);
 }
 catch (PDOException $e)
 {
